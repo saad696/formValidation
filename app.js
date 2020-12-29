661d6dc3d8059517965f8953d6137ecb266eaa45
Saad Shaikh <shksaad911@gmail.com>
2020-12-29 15:14:19 +0530

    bit of logic enhancement

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//DOM & variables deination
+//DOM & variables defination
 //sign up
 const signUpEmail = document.getElementById("signup-email");
 const signUpPwd = document.getElementById("signup-pwd");
@@ -15,7 +15,10 @@ const loginEmail = document.getElementById("login-email");
 const loginPwd = document.getElementById("login-pwd");
 const loginBtn = document.getElementById("login-btn");
 const lEmailHelp = document.getElementById("l-emailHelp");
-const lPasswordHelp = document.getElementById("l-passwordHelp");
+const lPasswordHelp = document.getElementById("l-passwordHelp") ;
+
+const mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+const pwdformat = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/;
 
 const users = [];
 
@@ -28,18 +31,24 @@ const signUpHandler = e => {
     password: signUpPwd.value,
   };
   if ( signUpUserName.value === "" || signUpUserName.value == null) {
+    signUpUserName.classList.add("is-danger")
     nameHelp.innerHTML = `<i class="fas fa-exclamation-circle"></i> Name is required.`
     return;
   } 
-  else if(!(signUpEmail.value.includes("@"))){
+  else if(!(signUpEmail.value.match(mailformat))){
+    signUpEmail.classList.add("is-danger")
     emailHelp.innerHTML = `<i class="fas fa-exclamation-circle"></i> Please enter a valid email.`
     return;
   }
-  else if(signUpPwd.value.length < 6){
-    passwordHelp.innerHTML = `<i class="fas fa-exclamation-circle"></i> Password must be between 8-20 characters.`
+  else if(!(signUpPwd.value.match(pwdformat))){
+    signUpPwd.classList.add("is-danger")
+    passwordHelp.innerHTML = `<i class="fas fa-exclamation-circle"></i> Password must be between 8-20 characters, and should includ one uppercase & one lowercase character & one number.`
     return;
   }  
   else{
+    signUpUserName.classList.remove("is-danger")
+    signUpEmail.classList.remove("is-danger")
+    signUpPwd.classList.remove("is-danger")
     nameHelp.innerHTML = "";
     emailHelp.innerHTML = "";
     passwordHelp.innerHTML = "";
@@ -50,25 +59,36 @@ const signUpHandler = e => {
    timeInterval(displayMsg);
    $("#signupForm").hide();
    $("#loginForm").show();
-  console.log(users);
+  console.log(users); 
+
 };
 
 const loginHandler = e => {
   e.preventDefault();
   users.every((uDetails) => {
-    if(loginEmail.value === "" || loginPwd.value === ""){
+    if(loginEmail.value === ""){
+      loginEmail.classList.add("is-danger")
       lEmailHelp.innerHTML = `<i class="fas fa-exclamation-circle"></i> Input required.`
-      lPasswordHelp.innerHTML = `<i class="fas fa-exclamation-circle"></i> Input required.`
       return;
     }
     else if(uDetails.email  !==  loginEmail.value){
+      loginEmail.classList.add("is-danger")
       lEmailHelp.innerHTML = `<i class="fas fa-exclamation-circle"></i> Invalid email.`
-    return true;
-    }else if(uDetails.password !== loginPwd.value){
+      return true;
+    }
+    else if(loginPwd.value === ""){
+        loginPwd.classList.add("is-danger")
+        lPasswordHelp.innerHTML = `<i class="fas fa-exclamation-circle"></i> Input required.`
+        return;
+      }
+    else if(uDetails.password !== loginPwd.value){
+      loginPwd.classList.add("is-danger")
       lPasswordHelp.innerHTML = `<i class="fas fa-exclamation-circle"></i> Invalid password.`
       return;
     }
     else{
+      loginEmail.classList.remove("is-danger")
+      loginPwd.classList.remove("is-danger")
       lEmailHelp.innerHTML = "";
       lPasswordHelp.innerHTML = "";
       loginEmail.value = "";
@@ -90,7 +110,7 @@ $('#loginForm').hide();
 $("#have-acc").click(function() {
   console.log('login btn clicked');
   $("#signupForm").hide();
-  $("#loginForm").show();
+  $("#loginForm").show() ;
 });
 
 $("#dont-have-acc").click(function() {
@@ -99,6 +119,6 @@ $("#dont-have-acc").click(function() {
   $("#signupForm").show();
 });
 
-//EventListeners.
+//EventListeners .
 signUpBtn.addEventListener("click", signUpHandler);
 loginBtn.addEventListener("click", loginHandler);
